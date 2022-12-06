5da10f36e383a93136afc952302b611614cf3e46
aquajr <ayandaobosi@gmail.com>
2022-12-06 17:46:20 +0300

    Add a login form

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/home')
 def index():
     """Index URL"""
     return render_template('index.html', title='Index Page')
@@ -16,9 +16,12 @@ def about_me():
     return render_template('about_me.html', title='About Me Page')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     """Login URL"""
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'You are requesting to login as{form.username.data}')
+        return redirect(url_for('/index'))
     return render_template('login.html', title='Login', form=form)
     
