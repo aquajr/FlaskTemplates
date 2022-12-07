400c7fe5d095cbe937a9c9b1cfeeabc3918e54ed
aquajr <ayandaobosi@gmail.com>
2022-12-07 17:43:58 +0300

    Adding a register form

--- app/forms.py
+++ app/forms.py
@@ -9,3 +9,12 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log in')
+
+
+class RegisterForm(FlaskForm):
+    '''Register Form'''
+    username = StringField('Username', validators=[DataRequired(), Length(1, 64)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
+    register = SubmitField('Register')
+    
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
+from app.forms import LoginForm, RegisterForm
 
 
 @app.route('/')
@@ -22,6 +22,16 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash(f'You are requesting to login as{form.username.data}')
-        return redirect(url_for('/index'))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """Register URL"""
+    form = RegisterForm()
+    if form.validate_on_submit():
+        flash(f'You are requesting to register as{form.username.data}')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
     
